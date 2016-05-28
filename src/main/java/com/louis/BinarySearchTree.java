edb69969fb7b234db68a4dabf23cf2e92a0ed227
Louis Ashton <louisashton@live.com>
2016-05-29 08:55:40 +1200

    Conforming to style.

--- src/main/java/com/louis/BinarySearchTree.java
+++ src/main/java/com/louis/BinarySearchTree.java
@@ -1,30 +1,34 @@
+// CHECKSTYLE:OFF
+
+// CHECKSTYLE:ON
+
 package com.louis;
 
-import java.util.NoSuchElementException;
 import com.google.common.base.Preconditions;
+import java.util.NoSuchElementException;
 
 /**
  * A binary search tree implementation.
- *
- * Binary search trees (BSTs) are derived from the associative array abstract data type. It is a
- * symbol table with keys and values; null is excluded. The tree is not balanced. Keys are
- * Comparable and the tree is sorted by them. Most operations have linear worst case complexity.
+ * <p>
+ * Binary search trees (BSTs) are derived from the associative array abstract data type. It is a symbol table with keys
+ * and values; null is excluded. The tree is not balanced. Ks are Comparable and the tree is sorted by them. Most
+ * operations have linear worst case complexity.
  *
  * @author Louis Ashton (louisashton@live.com)
  */
-public class BinarySearchTree<Key extends Comparable<Key>, Value> {
+public class BinarySearchTree<K extends Comparable<K>, V> {
 
     // The root of the binary search tree.
     private Node root;
 
     /**
      * A node of a binary search tree.
-     *
+     * <p>
      * Each node has a key and associated value. Pointers to the node's children are also stored.
      */
     private class Node {
-        private Key key;
-        private Value value;
+        private K key;
+        private V value;
         private Node leftSubtree;
         private Node rightSubtree;
         private int sizeOfSubtree;
@@ -32,7 +36,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         /**
          * Creates a Node.
          */
-        public Node(Key key, Value value, int sizeOfSubtree) {
+        Node(K key, V value, int sizeOfSubtree) {
             this.key = key;
             this.value = value;
             this.sizeOfSubtree = sizeOfSubtree;
@@ -47,7 +51,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     /**
      * Returns the number of nodes in the binary search tree.
      */
-    public int size() {
+    public final int size() {
         return size(root);
     }
 
@@ -55,7 +59,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Gets the size of a subtree.
      *
      * @param node the node is the root of the subtree.
-     * @return Returns the number of pairs descendant from a node.
+     * @return Returns the number of pairs descendant from a node
      */
     private int size(Node node) {
         if (node == null) {
@@ -69,11 +73,11 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Checks the binary search tree for a key.
      *
      * @param key the key that is to be found.
-     * @return true if key is in the binary search tree.
+     * @return true if key is in the binary search tree
      * @throws IllegalArgumentException if key is null.
      */
-    public boolean contains(Key key) {
-        Preconditions.checkArgument(key != null, "first argument to contains(Key key) is null");
+    public final boolean contains(K key) {
+        Preconditions.checkArgument(key != null, "first argument to contains(K key) is null");
         return get(key) != null;
     }
 
@@ -81,11 +85,11 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Gets a key's value.
      *
      * @param key the key whose value is sought.
-     * @return Returns the value of the key if it is in the binary search tree; null otherwise.
+     * @return Returns the value of the key if it is in the binary search tree; null otherwise
      * @throws IllegalArgumentException if key is null.
      */
-    public Value get(Key key) {
-        Preconditions.checkArgument(key != null, "First argument to get(Key key) is null.");
+    public final V get(K key) {
+        Preconditions.checkArgument(key != null, "First argument to get(K key) is null.");
         return get(root, key);
     }
 
@@ -93,10 +97,10 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Gets a key's value.
      *
      * @param node node is the root of the subtree.
-     * @param key  key is the key.
-     * @return Returns the value of the key if it is in the subtree; null otherwise.
+     * @param key key is the key.
+     * @return Returns the value of the key if it is in the subtree; null otherwise
      */
-    private Value get(Node node, Key key) {
+    private V get(Node node, K key) {
         if (node == null) {
             return null;
         }
@@ -113,13 +117,13 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     /**
      * Adds a node to the binary search tree.
      *
-     * @param key   the key to be added.
+     * @param key the key to be added.
      * @param value the value to be added.
      * @throws IllegalArgumentException if key is null.
      */
-    public void put(Key key, Value value) {
+    public final void put(K key, V value) {
         Preconditions.checkArgument(key != null,
-                "First argument to put(Key key, Value value) is null.");
+                "First argument to put(K key, V value) is null.");
         if (value == null) {
             delete(key);
             return;
@@ -130,12 +134,12 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     /**
      * Adds a pair to a subtree.
      *
-     * @param node  node is the root of the subtree.
-     * @param key   key is the key.
+     * @param node node is the root of the subtree.
+     * @param key key is the key.
      * @param value value is the value.
-     * @return Returns the updated root of the subtree.
+     * @return Returns the updated root of the subtree
      */
-    private Node put(Node node, Key key, Value value) {
+    private Node put(Node node, K key, V value) {
         if (node == null) {
             return new Node(key, value, 1);
         }
@@ -157,8 +161,8 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * @param key the key to be deleted.
      * @throws IllegalArgumentException if key is null.
      */
-    public void delete(Key key) {
-        Preconditions.checkArgument(key != null, "First argument to delete(Key key) is null.");
+    public final void delete(K key) {
+        Preconditions.checkArgument(key != null, "First argument to delete(K key) is null.");
         root = delete(root, key);
     }
 
@@ -166,10 +170,11 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Removes a key from a subtree.
      *
      * @param node node is the root of the subtree.
-     * @param key  key is the key.
-     * @return Returns the updated root of the subtree.
+     * @param key key is the key.
+     * @return Returns the updated root of the subtree
      */
-    private Node delete(Node node, Key key) {
+    private Node delete(Node node, K key) {
+        Node copyOfNode = node;
         if (node == null) {
             return null;
         }
@@ -179,17 +184,19 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         } else if (cmp > 0) {
             node.rightSubtree = delete(node.rightSubtree, key);
         } else {
-            if (node.rightSubtree == null)
+            if (node.rightSubtree == null) {
                 return node.leftSubtree;
-            if (node.leftSubtree == null)
+            } else if (node.leftSubtree == null) {
                 return node.rightSubtree;
-            Node topNode = node;
-            node = min(topNode.rightSubtree);
-            node.rightSubtree = deleteMin(topNode.rightSubtree);
-            node.leftSubtree = topNode.leftSubtree;
+            } else {
+                Node topNode = node;
+                copyOfNode = min(topNode.rightSubtree);
+                copyOfNode.rightSubtree = deleteMin(topNode.rightSubtree);
+                copyOfNode.leftSubtree = topNode.leftSubtree;
+            }
         }
-        node.sizeOfSubtree = size(node.leftSubtree) + size(node.rightSubtree) + 1;
-        return node;
+        copyOfNode.sizeOfSubtree = size(copyOfNode.leftSubtree) + size(copyOfNode.rightSubtree) + 1;
+        return copyOfNode;
     }
 
     /**
@@ -197,7 +204,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      *
      * @throws NoSuchElementException if the symbol table is empty
      */
-    public void deleteMin() {
+    public final void deleteMin() {
         if (size() == 0) {
             throw new NoSuchElementException("Nothing to delete.");
         }
@@ -208,7 +215,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Deletes a subtree's minimum.
      *
      * @param node node is the root of the subtree.
-     * @return Returns the updated root of the subtree.
+     * @return Returns the updated root of the subtree
      * @throws NoSuchElementException if the symbol table is empty
      */
     private Node deleteMin(Node node) {
@@ -223,10 +230,10 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     /**
      * Finds the minimum of the binary search tree.
      *
-     * @return Returns the smallest key in the symbol table.
+     * @return Returns the smallest key in the symbol table
      * @throws NoSuchElementException if the symbol table is empty.
      */
-    public Key min() {
+    public final K min() {
         if (size() == 0) {
             throw new NoSuchElementException("binary search tree is empty.");
         }
@@ -237,7 +244,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Finds the minimum of a subtree.
      *
      * @param node node is the root of the subtree.
-     * @return Returns the smallest key in the subtree.
+     * @return Returns the smallest key in the subtree
      */
     private Node min(Node node) {
         if (node.leftSubtree == null) {
@@ -250,10 +257,10 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     /**
      * Finds the binary search tree's maximum.
      *
-     * @return Returns the largest key in the symbol table.
+     * @return Returns the largest key in the symbol table
      * @throws NoSuchElementException if the symbol table is empty.
      */
-    public Key max() {
+    public final K max() {
         if (size() == 0) {
             throw new NoSuchElementException("The binary search tree is empty.");
         }
@@ -264,7 +271,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
      * Finds the maximum of a subtree.
      *
      * @param node node is the root of the subtree.
-     * @return Returns the largest key in the subtree.
+     * @return Returns the largest key in the subtree
      */
     private Node max(Node node) {
         if (node.rightSubtree == null) {
--- src/test/java/com/louis/BinarySearchTreeTest.java
+++ src/test/java/com/louis/BinarySearchTreeTest.java
@@ -1,6 +1,13 @@
+// CHECKSTYLE:OFF
+
+// CHECKSTYLE:ON
+
 package com.louis;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.NoSuchElementException;
 import org.junit.Test;
@@ -15,46 +22,46 @@ public class BinarySearchTreeTest {
     private BinarySearchTree<Integer, Integer> bst = new BinarySearchTree<>();
 
     @Test(expected = IllegalArgumentException.class)
-    public void canAddSearchAndDelete() {
+    public final void canAddSearchAndDelete() {
         assertEquals(0, bst.size());
-        assertEquals(null, bst.get(1));
+        assertNull(bst.get(1));
         bst.put(1, 2);
-        assertEquals(true, bst.contains(1));
+        assertTrue(bst.contains(1));
         assertEquals(1, bst.size());
-        assertEquals(false, bst.contains(2));
+        assertFalse(bst.contains(2));
         assertEquals(Integer.valueOf(2), bst.get(1));
         bst.delete(1);
-        assertEquals(false, bst.contains(1));
+        assertFalse(bst.contains(1));
         bst.put(1, 2);
         bst.put(1, null);
         bst.contains(null);
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void cannotDeleteFromEmptyTree() {
+    public final void cannotDeleteFromEmptyTree() {
         bst.deleteMin();
     }
 
     @Test
-    public void canRemoveBySettingNull() {
+    public final void canRemoveBySettingNull() {
         bst.put(1, 2);
-        assertEquals(true, bst.contains(1));
+        assertTrue(bst.contains(1));
         bst.put(1, null);
-        assertEquals(false, bst.contains(1));
+        assertFalse(bst.contains(1));
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void emptyTreeHasNoMax() {
+    public final void emptyTreeHasNoMax() {
         bst.max();
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void emptyTreeHasNoMin() {
+    public final void emptyTreeHasNoMin() {
         bst.min();
     }
 
     @Test
-    public void canMaintainOrder() {
+    public final void canMaintainOrder() {
         bst.delete(0);
         bst.put(1, 2);
         bst.put(2, 3);
